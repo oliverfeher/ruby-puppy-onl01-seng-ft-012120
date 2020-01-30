6d50fa26e9b6dcdc55e659d2dce0ec25ad8ecec1
Oliver Feher <olcsee@gmail.com>
2020-01-30 05:05:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,19 @@
-# Add your code here
+
 class Dog
   @@all = []
 
+  attr_accessor :name
+
   def initialize(name)
     @name = name
-    @@all << self
+    @@all << name
   end
   
   def self.all
     @@all
   end
-  
+
+  def self.print_all
+    @@all.map { |dog| puts dog}
+  end
 end
